4ba740699f1c2f2933701fe407b99a1f33c1b643
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 14:53:44 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -20,3 +20,24 @@ class Doctor
     Appointment.new( patient, date, self)
   end
 end 
+
+
+   
+   def self.all
+     @@all
+   end
+
+    def songs
+     Song.all.select {|song| song.artist == self}
+   end
+
+    def genres
+     songs.map {|song| song.genre}
+   end
+
+    def new_song(song, genre)
+     Song.new(name, self, genre)
+   end
+
+ 
+  end
