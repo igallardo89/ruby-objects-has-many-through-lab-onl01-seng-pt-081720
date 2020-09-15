950457d18d9f357b3b97822067babb3ec8c8c517
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 14:54:50 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,28 +16,14 @@ class Doctor
   def patients
    appointments.map {|appointment| appointment.patient}
   end
-  def new_appointment(patient, date)
-    Appointment.new( patient, date, self)
+  def new_appointment(patient, appointment)
+    Appointment.new(patient, self, appointment)
   end
 end 
 
 
    
-   def self.all
-     @@all
-   end
+   
 
-    def songs
-     Song.all.select {|song| song.artist == self}
-   end
 
-    def genres
-     songs.map {|song| song.genre}
-   end
-
-    def new_song(song, genre)
-     Song.new(name, self, genre)
-   end
-
- 
-  end
+   
