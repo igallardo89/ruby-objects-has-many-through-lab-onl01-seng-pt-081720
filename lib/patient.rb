457247dbd8142b8444d6b5133f76d022c6a68d26
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 15:25:05 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,27 +1,3 @@
 class Patient
 
-    attr_accessor :name, :doctor, :appointment
-   @@all = []
-
-    def initialize(name)
-     @name = name
-     @@all << self
-   end
-
-    def self.all
-     @@all
-   end
-
-    def appointments
-     Appointment.all.select {|appointment| appointment.patient == self}
-   end
-
-    def doctor
-     appointments.map {|appointment| appointment.doctor}
-   end
-
-    def new_appointment(doctor, appointment)
-     Appointment.new(self, doctor, appointment)
-   end
-
-  end
+ 
