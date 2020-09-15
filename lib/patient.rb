837f8824cc6071d481910a3bba9bd805436d161a
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 15:33:57 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,19 @@
 class Patient
+  attr_accessor :name, :doctor, :appointment
+  
+  @all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  
+end
   
   
 
