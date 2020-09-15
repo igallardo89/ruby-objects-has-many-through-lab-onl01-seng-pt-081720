7777004191c618388bc1ccc0cdbdfa7a410b1e9e
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 14:36:37 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -10,20 +10,9 @@ class Patient
    def self.all
      @@all
    end
-
-    def new_appointment(doctor, date)
+   def new_appointment(doctor, date)
      Appointment.new(self, doctor, date)
    end
 
-    def appointments
-     Appointment.all.select do |appointment|
-       appointment.patient == self
-     end
-   end
-
-    def doctors
-     Appointment.all.map do |appointment|
-       appointment.doctor
-     end
-   end
+   
  end
